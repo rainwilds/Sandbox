ed3a5ca58cfb26897c960c5aaaa5ba78fb1853b8
rainwilds <39784786+rainwilds@users.noreply.github.com>
2025-03-29 12:37:43 +1000

    x

--- scripts.js
+++ scripts.js
@@ -397,3 +397,14 @@ document.querySelectorAll('.modal-close').forEach(closeBtn => {
             modal.classList.remove('active');
         });
 });
+
+
+
+const showNumber = document.getElementById("showNumber");
+const favDialog = document.getElementById("favDialog");
+const number = document.getElementById("number");
+
+showNumber.addEventListener("click", () => {
+  number.innerText = Math.floor(Math.random() * 1000);
+  favDialog.showModal();
+});
