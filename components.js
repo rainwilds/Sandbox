0300b9fbfcabae87f5a89d1ad10c7c5bc6535f9c
rainwilds <39784786+rainwilds@users.noreply.github.com>
2025-04-13 23:00:58 +1000

    x

--- components.js
+++ components.js
@@ -14,21 +14,28 @@ class Footer extends HTMLElement {
 
   class Nav extends HTMLElement {
     connectedCallback() {
+      const hasDropdown = this.hasAttribute('dropdown') && this.getAttribute('dropdown') !== 'false';
+      const menuItemHtml = hasDropdown
+        ? `
+          <li class="dropdown-toggle">
+            <a href="./item2.html">Item 2</a>
+            <div class="dropdown-menu">
+              <a href="./item2/subitem1.html">Subitem 1</a>
+              <a href="./item2/subitem2.html">Subitem 2</a>
+            </div>
+          </li>
+        `
+        : `<li><a href="./item2.html">Item 2</a></li>`;
+  
       this.innerHTML = `
-        <nav>
+        <nav ${hasDropdown ? 'class="dropdown"' : ''}>
           <ul>
-            <li><a href="./index.html">Home</a></li>
-            <li>
-              Portfolio
-              <div>
-                <a href="./portfolio/photography.html">Photography</a>
-                <a href="./portfolio/video.html">Video</a>
-              </div>
-            </li>
-            <li><a href="./investment.html">Investment</a></li>
-            <li><a href="./blog.html">Blog</a></li>
-            <li><a href="./contact.html">Contact</a></li>
-            <li><a href="./about.html">About</a></li>
+            <li><a href="./item1.html">Item 1</a></li>
+            ${menuItemHtml}
+            <li><a href="./item3.html">Item 3</a></li>
+            <li><a href="./item4.html">Item 4</a></li>
+            <li><a href="./item5.html">Item 5</a></li>
+            <li><a href="./item6.html">Item 6</a></li>
           </ul>
           <i class="fa-sharp fa-light fa-bars font-size-medium"></i>
         </nav>
