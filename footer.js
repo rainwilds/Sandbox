79202b6dddc08509f3428b0613a91d5996bac815
rainwilds <39784786+rainwilds@users.noreply.github.com>
2025-04-13 14:00:05 +1000

    x

---
+++ footer.js
@@ -0,0 +1,13 @@
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <footer class="min-height-two-thirds">
+          <p>© ${new Date().getFullYear()} My Site. All rights reserved.</p>
+          <nav>
+            <a href="/about">About</a> | <a href="/contact">Contact</a>
+          </nav>
+        </footer>
+      `;
+    }
+  }
+  customElements.define('bh-footer', SiteFooter);
