9621636f486b0bae5c6bac140b1a9d6758f2d67d
rainwilds <39784786+rainwilds@users.noreply.github.com>
2025-04-20 18:24:44 +1000

    x

--- head.js
+++ head.js
@@ -293,7 +293,15 @@ function manageHead(attributes = {}, businessInfo = {}) {
           window.SnipcartSettings = {
             publicApiKey: 'NTMzMTQxN2UtNjQ3ZS00ZWNjLWEyYmEtOTNiNGMwNzYyYWNlNjM4ODA0NjY5NzE2NjExMzg5',
             loadStrategy: 'on-user-interaction',
-            version: '3.0'
+            version: '3.0',
+currency: 'aud',
+    timeoutDuration: 5000,
+    modalStyle: 'sidebar',
+    addProductBehavior: 'none',
+    allowedCountries: ['AU'],
+    defaultCountry: 'AU',
+    language: 'en'
+
           };
         `;
         document.body.appendChild(snipcartSettings);
