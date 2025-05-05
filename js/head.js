9df0b1bd4b73aa6f8e563c021c67889c85553890
rainwilds <39784786+rainwilds@users.noreply.github.com>
2025-05-05 23:25:24 +1000

    x

--- js/head.js
+++ js/head.js
@@ -401,7 +401,7 @@ function manageHead(attributes = {}, businessInfo = {}) {
   });
 
   // Load additional scripts (components.js, scripts.js)
-  const scripts = ['./js/components.js', './js/scripts.js'];
+  const scripts = ['.js/image-shared.js', './js/components.js', './js/scripts.js'];
   scripts.forEach(src => {
     if (!document.querySelector(`script[src="${src}"]`)) {
       const script = document.createElement('script');
